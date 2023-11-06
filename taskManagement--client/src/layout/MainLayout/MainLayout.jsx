bcfa97979831a49bf79c25c1049f46190f39110d
Tokiosman80F <tokiosman96@gmail.com>
2023-11-06 11:33:34 +0600

    client folder structure is completed

---
+++ taskManagement--client/src/layout/MainLayout/MainLayout.jsx
@@ -0,0 +1,13 @@
+import SideNavBar from "../../pages/SideNavBar/SideNavBar";
+
+
+const MainLayout = () => {
+    return (
+        <div>
+        
+            <SideNavBar></SideNavBar>
+        </div>
+    );
+};
+
+export default MainLayout;
