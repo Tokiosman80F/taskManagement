6b1d227a384279e01ab416593d1e27cc8d5dbe5d
Tokiosman80F <tokiosman96@gmail.com>
2023-11-06 16:24:17 +0600

    side nav bar completed

--- taskManagement--client/src/pages/SideNavBar/SideNavBar.jsx
+++ taskManagement--client/src/pages/SideNavBar/SideNavBar.jsx
@@ -1,11 +1,77 @@
-import React from 'react';
-
+import { useState } from "react";
+import { BiSolidLogOut } from "react-icons/bi";
+import {
+  FaAnglesLeft,
+  FaCirclePlus,
+  FaCircleUser,
+  FaIoxhost,
+  FaMoneyCheck,
+} from "react-icons/fa6";
 const SideNavBar = () => {
-    return (
-        <div>
-            The side bar
+  const [open, setOpen] = useState(true);
+  const menuItems = [
+    { title: "Task List", icon: <FaMoneyCheck />, link: "/" },
+    { title: "Add Task ", icon: <FaCirclePlus />, link: "/" },
+  ];
+
+  return (
+    <div className="">
+      <div
+        className={`${
+          open ? "w-72" : "w-20"
+        } duration-300 h-screen border border-gray-300 relative p-5 flex flex-col justify-evenly `}
+      >
+        <FaAnglesLeft
+          onClick={() => setOpen(!open)}
+          style={{ padding: "6px" }}
+          className={` ${
+            !open && `rotate-180 `
+          } text-3xl  bg-orange-400 absolute -right-3 top-3 cursor-pointer  rounded-full`}
+        />
+        {/* icon */}
+        <div className="flex  items-center ">
+          <div>
+            <FaIoxhost className="text-gray-600 text-2xl" />
+          </div>
+          <h1 className={`${!open && "scale-0"} font-bold text-2xl`}>
+            <span className="text-orange-500">Tasky</span>Mate
+          </h1>
         </div>
-    );
+        {/* user */}
+        <div className=" flex items-center gap-5 cursor-pointer text-white bg-orange-500 hover:bg-orange-400  p-2 rounded-lg duration-200 ">
+          <FaCircleUser />
+          <span className={` ${!open && `hidden duration-200`} font-semibold`}>
+            User
+          </span>
+        </div>
+        {/* menu */}
+        <div>
+          <ul>
+            {menuItems.map((item, index) => (
+              <li
+                key={index}
+                className="flex items-center  gap-5 cursor-pointer hover:bg-orange-400 hover:text-white p-2 rounded-lg duration-200"
+              >
+                {item.icon}
+                <span
+                  className={` ${!open && `hidden duration-200`} font-semibold`}
+                >
+                  {item.title}
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
+        {/* logout */}
+        <div className="flex items-center gap-5 cursor-pointer  hover:bg-orange-400  p-2 rounded-lg duration-200">
+          <BiSolidLogOut className="text-2xl" />
+          <span className={` ${!open && `hidden duration-200`} font-semibold`}>
+            Logout
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SideNavBar;
