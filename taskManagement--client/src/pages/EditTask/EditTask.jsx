7e53e1fcea91af97df14565f4ce2eb92763a74ef
Tokiosman80F <tokiosman96@gmail.com>
2023-11-07 20:27:35 +0600

    delete  functionality is working  properly

--- taskManagement--client/src/pages/EditTask/EditTask.jsx
+++ taskManagement--client/src/pages/EditTask/EditTask.jsx
@@ -3,7 +3,7 @@ import TopHeader from "../Shared/TopHeader";
 import Swal from "sweetalert2";
 
 const EditTask = () => {
-    const state = ["Active", "In Progress", "Pending", "Complete"];
+  const state = ["Active", "In Progress", "Pending", "Complete"];
   const [statuses, setStatuses] = useState([state[0]]);
   const handleSelect = (e) => {
     setStatuses(e.target.value);
@@ -28,93 +28,94 @@ const EditTask = () => {
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
-        if(data.insertedId)
-        {
+        if (data.insertedId) {
           Swal.fire({
             position: "center",
             icon: "success",
             title: "Your work has been saved",
             showConfirmButton: false,
-            timer: 2300
+            timer: 2300,
           });
-          form.reset()
+          form.reset();
         }
       });
   };
 
-
   return (
     <div className="w-full">
       <TopHeader>{"Edit Task"}</TopHeader>
-      <h2 className="text-center font-bold text-3xl my-5">
-        Update Your Task
-      </h2>
+      <h2 className="text-center font-bold text-3xl my-5">Update Your Task</h2>
+      <div
+        className="flex flex-col  
+                    items-center justify-center "
+      >
       <form className="w-full max-w-lg" onSubmit={handleSubmit}>
-          <div className="flex flex-wrap -mx-3 mb-6">
-            <div className="w-full  px-3 mb-6 md:mb-0">
-              <label className=" uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                Task Name
-              </label>
-              <input
-                required
-                className=" w-full bg-gray-200 text-gray-700 border border-orange-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                type="text"
-                name="taskName"
-              />
-            </div>
-            <div className="w-full  px-3">
-              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                Task Description
-              </label>
-              <textarea
-                required
-                name="description"
-                className="h-full min-h-[100px]  w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              ></textarea>
-            </div>
-          </div>
-
-          <div className="flex flex-wrap -mx-3 mb-2">
-            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                Date
-              </label>
-              <input
-                required
-                name="date"
-                className="  w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                type="date"
-              />
-            </div>
-            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-              <label className=" uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                State
-              </label>
-              <div>
-                <select
-                  required
-                  name="state"
-                  onChange={handleSelect}
-                  value={statuses}
-                  className=" w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                >
-                  {state.map((status) => (
-                    <option key={status} value={status}>
-                      {status}
-                    </option>
-                  ))}
-                </select>
-              </div>
-            </div>
-          </div>
-          {/* submit btn */}
-          <div className="w-full ">
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full  px-3 mb-6 md:mb-0">
+            <label className=" uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+              Task Name
+            </label>
             <input
-              type="submit"
-              className="w-full uppercase   bg-orange-400 hover:bg-orange-500 text-white border  rounded py-3 px-4 cursor-pointer "
-            ></input>
+              required
+              className=" w-full bg-gray-200 text-gray-700 border border-orange-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              type="text"
+              name="taskName"
+            />
           </div>
-        </form>
+          <div className="w-full  px-3">
+            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+              Task Description
+            </label>
+            <textarea
+              required
+              name="description"
+              className="h-full min-h-[100px]  w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            ></textarea>
+          </div>
+        </div>
+
+        <div className="flex flex-wrap -mx-3 mb-2">
+          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+              Date
+            </label>
+            <input
+              required
+              name="date"
+              className="  w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              type="date"
+            />
+          </div>
+          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label className=" uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+              State
+            </label>
+            <div>
+              <select
+                required
+                name="state"
+                onChange={handleSelect}
+                value={statuses}
+                className=" w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              >
+                {state.map((status) => (
+                  <option key={status} value={status}>
+                    {status}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+        </div>
+        {/* submit btn */}
+        <div className="w-full ">
+          <input
+            type="submit"
+            className="w-full uppercase   bg-orange-400 hover:bg-orange-500 text-white border  rounded py-3 px-4 cursor-pointer "
+          ></input>
+        </div>
+      </form>
+      </div>
     </div>
   );
 };
